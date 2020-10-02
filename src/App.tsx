78298e5950dfbfce26f24fbab983e372e39367f9
Chris Marshall <chris@codegoalie.com>
2020-10-02 08:37:36 -0400

    Save before removing typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
 import React from "react";
 import "./App.css";
+import { useImmer } from "use-immer";
+
+interface changeEvent {
+  target: valued
+}
+
+interface valued {
+  value: string
+}
+
+interface
+
+
 
 function App() {
+  const [inputs, setInputs] = useImmer(industryStandards);
+
+  const handleInput = (field: any, e: changeEvent) => {
+    setInputs((draft: ) => {
+      draft[field] = e.target.value;
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Calculate your Facebook Ad</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Calculate your Ad Spend</p>
+        <code>{JSON.stringify(inputs)}</code>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const industryStandards = {
+  revenueTarget: 50000,
+  salePrice: 10000,
+  salesCallConversionRate: 0.2,
+  cancellationRate: 0.3,
+  callBookingConversionRate: 0.05,
+  registrationPageConversionRate: 0.2,
+  cpc: 1,
+  ctr: 0.01,
+};
