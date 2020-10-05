84eba8982764868e964334c1ac4d1f0188f8625c
Chris Marshall <chris@codegoalie.com>
2020-10-05 08:17:26 -0400

    Start styleing results

--- src/ResultItem.js
+++ src/ResultItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import CheckGreen from "./check-green.svg";
+import CheckGrey from "./check-grey.svg";
+
 const ResultItem = ({ completed, label, value, description }) => {
   return (
     <div>
-      <span className="completed">{completed}</span>
+      <span className="completed">
+        <img
+          alt="Checkmark"
+          aria-hidden="true"
+          src={completed ? CheckGreen : CheckGrey}
+        />
+      </span>
       <span className="label">{label}</span>
       <span className="value">{value}</span>
       {description && <p>{description}</p>}
