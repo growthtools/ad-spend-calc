55265e038e0d2b591607796130fb6d1304c5f69b
Chris Marshall <chris@codegoalie.com>
2020-10-05 07:39:12 -0400

    Calculate completions for fields

---
+++ src/completion.js
@@ -0,0 +1,29 @@
+export const newCompletionFor = (currentCompletion, field) => {
+  const newCompletion = completionFor(field);
+
+  if (newCompletion < currentCompletion) {
+    return currentCompletion;
+  }
+
+  return newCompletion;
+};
+
+export const completionFor = field => {
+  completions[field] || 0;
+};
+
+export const isComplete = (currentCompletion, field) => {
+  return completionFor(field) <= currentCompletion;
+};
+
+const completions = {
+  revenueTarget: 1,
+  salePrice: 1,
+  haveSalesCall: 2,
+  salesCallConversionRate: 2,
+  cancellationRate: 2,
+  callBookingConversionRate: 3,
+  registrationPageConversionRate: 3,
+  cpc: 4,
+  ctr: 4,
+};
